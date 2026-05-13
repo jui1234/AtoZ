3f99568ee0e1d0de822cea5fd3994fd06a777c83
jui <juiyezarkar1999@gmail.com>
2026-05-13 13:02:55 +0530

    patterns

---
+++ patterns/NNumberPattern.js
@@ -0,0 +1,13 @@
+var NnumberPattern = function(n){
+
+for(let i=0;i<n;i++){
+  let star=' ';
+  for(let j=0;j<n;j++){
+    star+="*";
+  }
+  console.log(star)
+  // console.log("\n")
+}
+}
+
+NnumberPattern(5);
---
+++ patterns/pattern2.js
@@ -0,0 +1,13 @@
+var Pattern2 = function(n){
+
+for(let i=1;i<=n;i++){
+  let star='';
+  for(let j=1;j<=i;j++){
+    star+="*";
+  }
+  console.log(star)
+  // console.log("\n")
+}
+}
+
+Pattern2(5);
