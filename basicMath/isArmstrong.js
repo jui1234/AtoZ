c10c6f2f66a90b68f783f2e19cbd4378c00ff19b
jui <juiyezarkar1999@gmail.com>
2026-05-20 20:14:20 +0530

    isArmStrong.js

---
+++ basicMath/isArmstrong.js
@@ -0,0 +1,18 @@
+var isArmstrong = function(n){
+  let firstDigit=n/10;
+  let lastDigit=n%10;
+  let length=n.toString().length;
+   let sum=0;
+ 
+  for(let i=n;i>0;i=Math.floor(i/10)){
+    let lastDigit=i%10;
+    sum += Math.pow(lastDigit, length);
+  }
+ if(n===sum){
+   return true;
+ }
+ return false;
+}
+
+console.log(isArmstrong(12));
+console.log(isArmstrong(153));
