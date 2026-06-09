0ae49296828d6c107b6a1422928342d1798bb506
jui <juiyezarkar1999@gmail.com>
2026-06-09 19:34:59 +0530

    fix

--- sorting/insertionSort.js/insertionSort.js
+++ sorting/insertionSort.js/insertionSort.js
@@ -1,7 +1,7 @@
 let insertionSort=function(nums){
     for(let i=1;i<nums.length;i++){
         let key=nums[i];
-        j=i-1;
+       let  j=i-1;
 
         while(j>=0 && nums[j]>key){
             nums[j+1]=nums[j];
