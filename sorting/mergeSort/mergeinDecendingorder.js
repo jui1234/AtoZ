52e47d5e8e74d7371f0639e753b79e01b37746c9
jui <juiyezarkar1999@gmail.com>
2026-06-10 19:19:04 +0530

    decending meregre

---
+++ sorting/mergeSort/mergeinDecendingorder.js
@@ -0,0 +1,40 @@
+let mergeSort=function(arr){
+    if(arr.length<=1){
+        return arr;
+    }
+    let mid=Math.floor(arr.length/2);
+   let left = mergeSort(arr.slice(0, mid));
+    let right = mergeSort(arr.slice(mid));
+
+
+    return mergedecending(left,right);
+}
+
+let mergedecending=function(left,right){
+let i=0;
+let j=0;
+let result=[];
+
+while(i<left.length && j<right.length){
+    if(left[i]>=right[j]){
+          result.push(left[i]);
+          i++;
+    }
+    else{
+        result.push(right[j])
+        j++;
+    }
+}
+while(i<left.length){
+    result.push(left[i])
+    i++;
+}
+while(j<right.length){
+    result.push(right[j])
+    j++;
+}
+return result;
+}
+
+
+console.log("mergeSort",mergeSort([3,4,2,6,7,1]))
