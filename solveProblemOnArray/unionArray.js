7a81af9396e8ccc0e97512e36d3d7e287a5c0ff0
jui <juiyezarkar1999@gmail.com>
2026-06-17 18:11:37 +0530

    union array

---
+++ solveProblemOnArray/unionArray.js
@@ -0,0 +1,19 @@
+let unionArray=function(nums1,nums2){
+   for(let i=0;i<nums2.length;i++){
+    let found=false;
+
+    for(let j=0;j<nums1.length;j++){
+        if(nums1[j]===nums2[i]){
+            found=true;
+            break;
+        }
+    }
+    if(!found){
+        nums1.push(nums2[i])
+    }
+          
+   } 
+    return nums1;
+}
+
+console.log("unionArray",unionArray([1,2,3,4],[3,4,5,6]))
