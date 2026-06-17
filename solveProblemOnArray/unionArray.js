09fe256ddf03f7602b0238ebe6fdb9753a5d9b85
jui <juiyezarkar1999@gmail.com>
2026-06-17 18:30:50 +0530

    added the sort function

--- solveProblemOnArray/unionArray.js
+++ solveProblemOnArray/unionArray.js
@@ -17,3 +17,4 @@ let unionArray=function(nums1,nums2){
 }
 
 console.log("unionArray",unionArray([1,2,3,4],[3,4,5,6]))
+console.log("unionArray",unionArray([3, 4, 6, 7, 9, 9],[1, 5, 7, 8, 8]))
--- solveProblemOnArray/unionArrayUsingSets.js
+++ solveProblemOnArray/unionArrayUsingSets.js
@@ -1,5 +1,6 @@
 let unionArray=function(nums1,nums2){
- return [...new Set([...nums1,...nums2])];    
+ return [...new Set([...nums1,...nums2])].sort();    
 }
 
 console.log("unionArray",unionArray([1,2,3],[3,4,5,6]))
+console.log("unionArray",unionArray([3, 4, 6, 7, 9, 9],[1, 5, 7, 8, 8]))       
