081113788a713e3a4d693f2e21ef20c465f5d29f
jui <juiyezarkar1999@gmail.com>
2026-07-02 19:07:17 +0530

    maxXorReturnArrya

---
+++ solveProblemOnArray/maxXorREturnArray.js
@@ -0,0 +1,20 @@
+let subarraysWithXorK=function(nums,k){
+    let mainresult=[];
+    let maxCount=0;
+
+    for(let i=0;i<nums.length;i++){
+        let result=[];
+        let xor=0;
+        for(let j=i;j<nums.length;j++){
+            xor^=nums[j];
+            result.push(nums[j]);
+            if(xor===k && result.length>maxCount){
+                maxCount=result.length-1;
+                mainresult=[...result];
+            }
+        }
+    }
+    return mainresult;
+}
+console.log("subarraysWithXorK",subarraysWithXorK([4,2,2,6,4],6))
+console.log("subarraysWithXorK",subarraysWithXorK( [5, 6, 7, 8, 9],5))
