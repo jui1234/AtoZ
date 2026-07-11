95ea61a3178ec7d4f629942669ccb63dd55717af
jui <juiyezarkar1999@gmail.com>
2026-07-11 20:17:24 +0530

    search range better approach

---
+++ BSof1DArray/searchRangeBetterApproach.js
@@ -0,0 +1,20 @@
+let searchRange=function(nums,target){
+   let first=-1;
+   let last=-1;
+
+   for(let i=0;i<nums.length;i++){
+    if(nums[i]===target){
+        if(first===-1){
+            first=i;
+        }
+        last=i;
+    }
+   }
+
+   return [first,last];
+
+}
+
+console.log("searchRange",searchRange([5, 7, 7, 8, 8, 10],8))
+console.log("searchRange",searchRange([5, 7, 7, 8, 8, 10],6))
+console.log("searchRange",searchRange([5, 7, 7, 8, 8, 10],5))
