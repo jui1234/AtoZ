3a0cd27b5bff7d5920752173379be213babcb3a0
jui <juiyezarkar1999@gmail.com>
2026-08-08 18:11:40 +0530

    String to Integer (atoi)

---
+++ StringProblem/StringtoInteger(atoi).js
@@ -0,0 +1,17 @@
+
+let myAtoi=function(input){
+let sign=1;
+let CN=0;
+for(let i=0;i<input.length;i++){
+    if(input[i]==="-"){
+        sign=-1;
+    }
+
+    if(input[i]>="0" && input[i]<="9"){
+      CN=CN*10+Number(input[i]);
+    }
+    
+}
+return sign*CN;
+}
+console.log("myAtoi",myAtoi("-1234"))
