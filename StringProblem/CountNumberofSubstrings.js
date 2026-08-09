94d3c05e67c2685c6d3da0270ee0a2b136cfb316
jui <juiyezarkar1999@gmail.com>
2026-08-09 16:36:01 +0530

    countNumber od subscrting using K

--- StringProblem/CountNumberofSubstrings.js
+++ StringProblem/CountNumberofSubstrings.js
@@ -1,33 +1,27 @@
 let NumberofSubstrings=function(s,k){
+  let count=0;
+
+  for(let i=0;i<s.length;i++){
     let charcount=0;
-    let count=0;
-    for(let i=0;i<s.length;i++){
     let map=new Map();
-        let str="";
-        for(let m=0;m<=k+i;m++){
-            if(map.has(s[m]) && charcount<k){
-              count++;
-              str+=s[m];
-              map.set(s[m],map.get(s[m]||0)+1)
-              charcount++;
-            }
-            else if(charcount>k){
-                break;
-            }
+
+    for(let j=i;j<s.length;j++){
+        if(!map.has(s[j])){
+            charcount++;
         }
-        for(let j=i+k;j<s.length;j++){
-             if(map.has(s[j]) && charcount<k){
-                count++;
-              str+=s[j];
-              map.set(s[j],map.get(s[j]||0)+1)
-              charcount++;
-            }
-            else if (charcount>k){
-                break;
-            }
+
+        map.set(s[j],map.get(s[j]||0)+1);
+
+        if(charcount===k){
+            count++;
+        }
+
+        if(charcount>k){
+            break;
         }
     }
-    return count;
+  }
+  return count;
 }
 
 console.log("NumberofSubstrings",NumberofSubstrings("pqpqs",2))
