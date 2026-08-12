394d7ab23efeeffb128e4cccab0b9b92f0566aa2
jui <juiyezarkar1999@gmail.com>
2026-08-12 19:28:29 +0530

    link list basic

--- linkList/BasicOfLinkLIst.js
+++ linkList/BasicOfLinkLIst.js
@@ -5,14 +5,24 @@ class Node{
     }
 }
 
+ class LinkList{
+    constructor()
+    {
+        this.head=null;
+    }
+ }
+
+ const head=new Node(10,null);
 const node1=new Node(100,200);
 const node2=new Node(200,300);
 const node3=new Node(300,null);
 
 node1.next=node2;
 node2.next=node3;
+head.next=node1;
 console.log("node1",node1);
 console.log("node2",node2);
 console.log("node3",node3);
 console.log("connected data ",node2.next.data);
 console.log("connected data ",node3.next);
+console.log("head",head);
