f2b2f7dbcf805ae9f2beb7faf96d39215a6ac4ba
jui <juiyezarkar1999@gmail.com>
2026-08-13 12:54:17 +0300

    link list basic with header

--- linkList/BasicOfLinkLIst.js
+++ linkList/BasicOfLinkLIst.js
@@ -1,28 +1,53 @@
-class Node{
-    constructor(data,next){
-        this.data=data;
-        this.next=next;
+// class Node{
+//     constructor(data,next){
+//         this.data=data;
+//         this.next=next;
+//     }
+// }
+
+//  class LinkList{
+//     constructor()
+//     {
+//         this.head=null;
+//     }
+//  }
+
+//  const head=new Node(10,null);
+// const node1=new Node(100,200);
+// const node2=new Node(200,300);
+// const node3=new Node(300,null);
+
+// node1.next=node2;
+// node2.next=node3;
+// head.next=node1;
+// console.log("node1",node1);
+// console.log("node2",node2);
+// console.log("node3",node3);
+// console.log("connected data ",node2.next.data);
+// console.log("connected data ",node3.next);
+// console.log("head",head);
+
+class LinkList{
+    constructor(head){
+        this.head=head;
     }
 }
 
- class LinkList{
-    constructor()
-    {
-        this.head=null;
+class Node{
+    constructor(node){
+        this.node=node;
+        // this.next=null;
+
     }
- }
+}
 
- const head=new Node(10,null);
-const node1=new Node(100,200);
-const node2=new Node(200,300);
-const node3=new Node(300,null);
+let node1=new Node(10);
+let node2=new Node(20);
 
+const list=new LinkList();
+list.head=node1;
 node1.next=node2;
-node2.next=node3;
-head.next=node1;
-console.log("node1",node1);
-console.log("node2",node2);
-console.log("node3",node3);
-console.log("connected data ",node2.next.data);
-console.log("connected data ",node3.next);
-console.log("head",head);
+
+console.log("list",list.head)
+console.log("list",node1.next)
+
