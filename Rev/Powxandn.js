5f5aa225e2a04cd4ecbf8b667c83862fcb693a6d
jui <juiyezarkar1999@gmail.com>
2026-09-01 20:12:51 +0530

    powXandn

--- Rev/Powxandn.js
+++ Rev/Powxandn.js
@@ -2,13 +2,10 @@ let myPow=function(x,n){
     if(n===0){
         return 1;
     }
-
-     if (n < 0) {
-            return 1 / myPow(x, -n);
-        }
-
-    return x*myPow(x,n-1);
+    if(n<=0){
+        return x/myPow(x,n-1);
+    }
+    return x*myPow(x,n-1)
 }
 
-console.log("myPow",myPow(2,10));
-console.log("myPow",myPow(2,-2));
+console.log("myPow",myPow(2,10))
